fcda9ea569389bb07a015210c1b66ceee75adeea
Tom Lazar <thomas.lazar@marquette.edu>
2017-08-08 10:51:55 -0500

    got a simple way to start walking down throught the ast nodes going

--- src/diff.py
+++ src/diff.py
@@ -1,9 +1,49 @@
 import ast
+import re
 
 sample = """
+
+def test1():
+    print("This world")
+
+def test2():
+    print("or the next")
+
 print("Hello, World")
 """
 
+
 tree = ast.parse(sample)
 
-print(ast.dump(tree))
+print("\nAST Dump:")
+print(ast.dump(tree) + "\n")
+
+tab = 0
+
+
+def ptab():
+    global tab
+    o = ['\t' for x in range(0, tab)]
+    print("".join(o), end="")
+
+
+def tabin():
+    global tab
+    tab += 1
+
+
+def tabdown():
+    global tab
+    tab -= 1
+
+
+def rec_down(node):
+    for fieldname, value in ast.iter_fields(node):
+        print(f"{type(node)} -> ")
+        tabin()
+        ptab()
+        print(f"{fieldname}: {value}")
+
+
+print("\nNode Traversal:")
+rec_down(tree)
